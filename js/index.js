702a8051a0ef16bd0f7d4dbfd42f6930c5cdcbb4
aleph ramos <alephramos142857@gmail.com>
2023-11-23 16:13:35 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+// Função para animar a digitação
 function typeWriter() {
     const texto = "Bem vindo(a) ao meu site!";
     let i = 0;
-    const speed = 0; // Velocidade da digitação em milissegundos
+    const speed = 50; // Velocidade da digitação em milissegundos
 
     function type() {
         if (i < texto.length) {
@@ -14,49 +15,47 @@ function typeWriter() {
     type();
 }
 
-// Inicia a animação quando a página é carregada
-window.onload = function() {
-    typeWriter();
-};
-
-
-window.addEventListener('DOMContentLoaded', function() {
+// Função para mostrar gradativamente a imagem "sobre mim"
+function showSobreMimImg() {
     var sobreMimImg = document.getElementById('sobreMimImg');
     
     setTimeout(function() {
-        sobreMimImg.classList.add('show'); // Adiciona a classe 'show' para tornar a imagem visível gradativamente
-    }, 400); // 1 segundo
-});
-
-
-const icons = document.querySelectorAll('.img-bar');
-
-function handleAnimation() {
-    icons.forEach(icon => {
-        icon.style.animationPlayState = 'running'; // inicia a animação
-    })
+        sobreMimImg.classList.add('show');
+    }, 400); // 400 milissegundos (1 segundo)
 }
 
-window.addEventListener('load', handleAnimation);
+// Função para iniciar a animação quando a página é carregada
+function handleAnimationOnLoad() {
+    const icons = document.querySelectorAll('.img-bar');
 
+    icons.forEach(icon => {
+        icon.style.animationPlayState = 'running';
+    });
+}
 
-
-
-
-
+// Função para verificar se um elemento está visível na tela
 function isElementInViewport(element) {
     var rect = element.getBoundingClientRect();
     return rect.top <= (window.innerHeight || document.documentElement.clientHeight);
-  }
-  
-  function handleScroll() {
+}
+
+// Função para lidar com o evento de rolar a página
+function handleScrollAnimation() {
     var elementos = document.querySelectorAll('.animacao');
     
     elementos.forEach(function(elemento) {
-      if (isElementInViewport(elemento)) {
-        elemento.classList.add('aparecer');
-      }
+        if (isElementInViewport(elemento)) {
+            elemento.classList.add('aparecer');
+        }
     });
-  }
-  
-  window.addEventListener('scroll', handleScroll);
+}
+
+// Executar funções quando a página é carregada
+window.onload = function() {
+    typeWriter(); // Iniciar a animação de digitação
+    showSobreMimImg(); // Mostrar gradativamente a imagem "sobre mim"
+    handleAnimationOnLoad(); // Iniciar a animação dos ícones
+};
+
+// Adicionar um ouvinte de evento para lidar com o evento de rolar a página
+window.addEventListener('scroll', handleScrollAnimation);
